a892fa5c81a469b3f8079823e7a3d084abf5ff6f
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-09 11:12:01 +0700

    feat: update start script

--- start.sh
+++ start.sh
@@ -3,15 +3,19 @@
 echo "Starting Docker Compose services..."
 docker-compose up -d
 
-# Navigate to the React frontend directory and start the development server
 echo "Starting React frontend..."
 cd ./frontend
 npm install
 npm start &
 
-# Navigate to the Flask backend directory and start the Flask server
 echo "Starting Flask backend..."
-cd ../backend  # Go back to the root, then into the backend directory
+cd ../backend
+
+if [ ! -d "venv" ]; then
+  echo "Virtual environment not found, creating one..."
+  python3 -m venv venv
+fi
+
 source venv/bin/activate
 pip install -r requirements.txt
 export FLASK_APP=app.py
