e5ba0f0130c0753a8f15f3861b9da05ce68116c8
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-11 19:53:06 +0700

    fix: fix dev enviroments

--- start.sh
+++ start.sh
@@ -2,15 +2,20 @@
 
 docker-compose up -d
 
-cd ./frontend || exit
+cd ./frontend || exit 1
 npm install
 npm start &
 
-cd ../backend || exit
+cd ../backend || exit 1
+
 if [ ! -d "venv" ]; then
   python3 -m venv venv
 fi
+
 source venv/bin/activate
+
 pip install -r requirements.txt
+
 export FLASK_APP=app.py
-flask run &
+
+flask run --host=0.0.0.0 --port=5001 &
