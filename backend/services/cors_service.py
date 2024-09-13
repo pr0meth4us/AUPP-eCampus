2d83cb95e693a9a85ea0e9e9f218e3f6802b546d
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-13 12:11:12 +0700

    remodulize structure

---
+++ backend/services/cors_service.py
@@ -0,0 +1,8 @@
+from flask_cors import CORS
+
+cors = CORS()
+
+
+def init_cors(app):
+    allowed_origins = app.config['CORS_ALLOWED_ORIGINS']
+    cors.init_app(app, resources={r"/*": {"origins": allowed_origins}})
