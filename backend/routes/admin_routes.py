acf74ade3632e3ee61c23362b85ac7e2adbfe77c
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-16 18:40:30 +0700

    feat: admin register

--- backend/routes/admin_routes.py
+++ backend/routes/admin_routes.py
@@ -1,10 +1,12 @@
-from flask import jsonify, Blueprint
+from flask import Blueprint, request
 from middleware.admin_middleware import require_admin
+from controllers.auth_controller import register_user
 
 admin_bp = Blueprint('admin', __name__)
 
 
 @admin_bp.route('/instructor-register', methods=['POST'])
 @require_admin
-def admin_only_route():
-    return jsonify({'message': 'Welcome, Admin!'})
+def register_instructor():
+    data = request.get_json()
+    return register_user('instructor', data)
--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -4,10 +4,16 @@ from controllers.auth_controller import register_user, login_user
 auth_bp = Blueprint('auth', __name__)
 
 
-@auth_bp.route('/register', methods=['POST'])
-def register():
+@auth_bp.route('/register/student', methods=['POST'])
+def register_student():
     data = request.get_json()
-    return register_user(data)
+    return register_user('student', data)
+
+
+@auth_bp.route('/register/admin', methods=['POST'])
+def register_admin():
+    data = request.get_json()
+    return register_user('admin', data)
 
 
 @auth_bp.route('/login', methods=['POST'])
