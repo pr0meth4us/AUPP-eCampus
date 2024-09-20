231e47ea7be0aaafc71cc2972a7c707effd31a6c
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-20 10:56:38 +0700

    modify

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,15 +2,19 @@ import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className="banner bg-primary d-flex justify-content-center ">
+        <div className="banner bg-primary d-flex justify-content-center">
             <div>
-                <Link className="text-white" to="https://www.aupp.edu.kh/">AUPP |</Link>
+                <Link className="text-white" to="https://www.aupp.edu.kh/">AUPP
+                    <span>&nbsp;|&nbsp;</span>
+                    </Link>
             </div>
             <div>
-                <Link className="text-white" to="/"> News & Events |</Link>
+            <Link className="text-white" to="/">News & Events
+                    <span>&nbsp;|&nbsp;</span>
+                </Link>
             </div>
             <div>
-                <Link className="text-white" to="https://www.aupp.edu.kh/student-life/"> Student Life</Link>
+            <Link className="text-white" to="https://www.aupp.edu.kh/student-life/">Student Life</Link>
             </div>
         </div>
     );
