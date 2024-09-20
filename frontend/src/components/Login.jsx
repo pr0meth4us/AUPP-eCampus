41a345723cdb20316fab6aef7e3ddf3e75adf921
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-20 08:54:31 +0700

    trigger close sign in modal after sign in successfully

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { useAuth } from '../context/authContext';
 import { useNavigate } from 'react-router-dom';
 
