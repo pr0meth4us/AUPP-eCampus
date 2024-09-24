362714e62d0a9cc1db95fa5a523cf5991e0aabd6
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-24 19:39:05 +0700

    set up otp

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,4 @@
 from flask import Flask, jsonify
-from routes.auth_routes import auth_bp
-from routes.admin_routes import admin_bp
-from services.cors_service import init_cors
 from services.mongo_service import init_mongo
 from config import Config
 import traceback
@@ -11,17 +8,14 @@ def create_app():
     flask_app = Flask(__name__)
 
     flask_app.config.from_object(Config)
-    print(init_cors)
-
-    init_cors(flask_app)
     init_mongo(flask_app)
+    from services.cors_service import init_cors
+    init_cors(flask_app)
 
+    from routes.auth_routes import auth_bp
+    from routes.admin_routes import admin_bp
     flask_app.register_blueprint(auth_bp, url_prefix='/auth')
     flask_app.register_blueprint(admin_bp, url_prefix='/admin')
-
-    @flask_app.route('/test', methods=['GET'])
-    def test_endpoint():
-        return jsonify({'message': 'Test endpoint is working. Server is up!'}), 200
 
     @flask_app.errorhandler(500)
     def internal_error(error):
