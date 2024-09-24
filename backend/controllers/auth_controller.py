91f0a902cbb1dcb11cb98152248656b30cd371a3
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-09-24 19:51:45 +0700

    finish set up otp

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -1,5 +1,4 @@
 import time
-from random import randint
 import jwt
 from flask import jsonify, make_response, request
 from config import Config
@@ -7,32 +6,16 @@ from models.authentication_model import Authentication
 from models.user_model import User, Student, Admin, Instructor
 from services.mail_service import send_mail
 from utils.token_utils import create_token
+from models.otp_model import OTP
 
-otp_storage = {}
 
-
-def verify_otp(data):
+def check_email(data):
     email = data.get('email')
-    received_otp = data.get('otp')
-
-    if email in otp_storage:
-        entry = otp_storage[email]
-        if entry['otp'] == int(received_otp) and (time.time() - entry['timestamp'] < 300):
-            del otp_storage[email]
-            return True
-    return False
-
-
-def trigger_send_otp(data):
-    email = data.get('email')
-
     if User.find_by_email(email):
         return jsonify({'message': 'Email already exists.'}), 409
 
-    email = data.get('email')
-    otp = randint(10000, 99999)
+    otp = OTP.create_otp(email)
     send_mail(email, otp)
-    otp_storage[email] = {'otp': otp, 'timestamp': time.time()}
     return jsonify({'message': 'OTP sent to your email.'}), 200
 
 
@@ -41,14 +24,10 @@ def register(data):
     received_otp = data.get('otp')
     role = data.get('role')
 
-    if verify_otp({'email': email, 'otp': received_otp}):
-        user_classes = {
-            'student': Student,
-            'instructor': Instructor,
-            'admin': Admin
-        }
-
+    if OTP.verify_otp(email, int(received_otp)):
+        user_classes = {'student': Student, 'instructor': Instructor, 'admin': Admin}
         user_class = user_classes.get(role)
+
         if role == 'admin' and data.get('token') != Config.ADMIN_TOKEN:
             return jsonify({'message': 'Invalid admin token.'}), 403
 
@@ -61,10 +40,8 @@ def register(data):
 
 def login_user(data):
     role = data.get('role')
-
     if not role or role not in ['student', 'instructor', 'admin']:
-        return jsonify(
-            {'message': 'You must specify whether you are logging in as student, instructor, or admin.'}), 400
+        return jsonify({'message': 'You must specify whether you are logging in as student, instructor, or admin.'}), 400
 
     user = User.find_by_email_and_role(data['email'], role)
     if user and User.verify_password(user['password_hash'], data['password']):
@@ -76,11 +53,7 @@ def login_user(data):
         response = make_response(jsonify({
             'message': 'Login successful',
             'token': token,
-            'user': {
-                '_id': user_id_str,
-                'email': user['email'],
-                'role': role
-            }
+            'user': {'_id': user_id_str, 'email': user['email'], 'role': role}
         }), 200)
         response.set_cookie('auth_token', token, httponly=True, secure=True)
 
@@ -98,21 +71,14 @@ def check_auth():
     try:
         decoded_token = jwt.decode(token, Config.SECRET_KEY, algorithms=['HS256'])
         user_id = decoded_token['_id']
-    except jwt.ExpiredSignatureError:
-        return jsonify({"authenticated": False, "message": "Token expired"}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({"authenticated": False, "message": "Invalid token"}), 401
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        return jsonify({"authenticated": False, "message": "Invalid or expired token"}), 401
 
     user = User.find_by_id(user_id)
     if not user:
         return jsonify({"authenticated": False, "message": "User not found"}), 401
 
-    return jsonify({
-        "authenticated": True,
-        "user": {
-            "role": user['role']
-        }
-    }), 200
+    return jsonify({"authenticated": True, "user": {"role": user['role']} }), 200
 
 
 def logout():
