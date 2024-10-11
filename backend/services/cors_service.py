a806df9ace775c4311083acaf113dc4dfed8c6d8
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-11 10:05:31 +0700

    fix: debug log in

--- backend/services/cors_service.py
+++ backend/services/cors_service.py
@@ -5,9 +5,9 @@ cors = CORS()
 
 
 def init_cors(app):
-    allowed_origins = Config.CORS_ALLOWED_ORIGINS
     cors.init_app(app, resources={r"/*": {
-        "origins": allowed_origins,
+        "origins": "*",
         "supports_credentials": True
     }})
-    print(f"CORS allowed origins: {allowed_origins}")
+    print("CORS allowed origins: All (*)")
+
