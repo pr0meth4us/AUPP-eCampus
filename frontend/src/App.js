eeff11991510d9432a75d9c2b6864208d4c77be6
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-13 23:44:01 +0700

    feat: modify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/Home/Login';
 import AdminPage from './pages/AdminPage';
 import PrivateRoute from './components/PrivateRoute';
-import Signup from "./components/Home/Signup";
 import Home from "./pages/Home";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import AdminSignup from "./components/Admin/AdminSignup";
@@ -15,9 +13,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/admin-signup" element={<AdminSignup />} />
-                <Route path="/signup" element={<Signup />} />
                 <Route path="/admin-login" element={<AdminLogin />} />
-                <Route path="/login" element={<LoginPage />} />
                 <Route path="/admin/dashboard" element={<PrivateRoute element={<AdminPage />} allowedRoles={['admin']} />} />
             </Routes>
         </Router>
