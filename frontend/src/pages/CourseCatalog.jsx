edaaeccc925ec1a187b52bc9bf1e08d1944f3a81
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-16 23:13:59 +0700

    feat: course catalog

--- frontend/src/pages/CourseCatalog.jsx
+++ frontend/src/pages/CourseCatalog.jsx
@@ -2,14 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getAllCourses } from "../services/api";
 import Header from "../components/Home/Header";
 import Footer from "../components/Footer";
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button
-} from "@material-tailwind/react";
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const CourseCatalog = () => {
     const [courses, setCourses] = useState([]);
@@ -37,28 +30,23 @@ const CourseCatalog = () => {
     return (
         <>
             <Header />
-            <div className="flex flex-wrap justify-center gap-6 p-4">
-                {courses.map(course => (
-                    <Card key={course.id} className="mt-6 w-96">
-                        <CardHeader color="blue-gray" className="relative h-56">
-                            <img
-                                src={course.thumbnail_url}
-                                alt={`Thumbnail for ${course.title}`}
-                                className="w-full h-full object-cover"
-                            />
-                        </CardHeader>
-                        <CardBody>
-                            <Typography variant="h5" color="blue-gray" className="mb-2">
-                                {course.title}
-                            </Typography>
-                            <Typography>{course.description}</Typography>
-                        </CardBody>
-                        <CardFooter className="pt-0">
-                            <Button>Learn More</Button>
-                        </CardFooter>
-                    </Card>
-                ))}
-            </div>
+            <Container>
+                <Row className="g-4">
+                    {courses.map(course => (
+                        <Col key={course.id} xs={12} md={6} lg={4}>
+                            <Card>
+                                <Card.Img variant="top" src={course.thumbnail_url} alt={`Thumbnail for ${course.title}`} />
+                                <Card.Body>
+                                    <i className="bi bi-person-workspace text-muted"></i><small className="p-2 text-muted">{course.instructor_name}</small>
+                                    <Card.Title>{course.title}</Card.Title>
+                                    <Card.Text>{course.description}</Card.Text>
+                                    <Button variant="primary" href={course.video_url} target="_blank">Watch</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
             <Footer />
         </>
     );
