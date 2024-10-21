3224f049863b1926b8f1a0f1f30a8c5c67491063
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-21 13:29:08 +0700

    fix: tcp health check

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,6 @@
-let API_BASE_URL;
-
-if (process.argv.includes('-prod')) {
-    API_BASE_URL = 'https://thundering-sharai-aupp-156f29b2.koyeb.app/';
-} else {
-    API_BASE_URL = 'http://localhost:5001';
-}
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||
+    (process.env.REACT_APP_ENV === 'production'
+        ? 'https://thundering-sharai-aupp-156f29b2.koyeb.app/'
+        : 'http://localhost:5001');
 
 export { API_BASE_URL };
