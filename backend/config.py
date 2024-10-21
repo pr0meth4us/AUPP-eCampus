5d50577812b2b8d010c692adb71a59b3e3b91a35
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-21 12:29:37 +0700

    fix: docker

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,11 @@
 import os
-
+import random
+import time
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
+from utils.logger import logger
 
 class Config:
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/mydb')
@@ -14,7 +20,13 @@ class Config:
     JWT_TOKEN_LOCATION = ['headers']
     JWT_HEADER_NAME = 'Authorization'
     JWT_HEADER_TYPE = 'Bearer'
-    GOOGLE_SERVICE_ACCOUNT_FILE = os.getenv('GOOGLE_SERVICE_ACCOUNT_FILE', 'youtube-credentials.json')
-    CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
-    CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
-    CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
+    GOOGLE_SERVICE_ACCOUNT_TYPE = os.getenv('GOOGLE_SERVICE_ACCOUNT_TYPE')
+    GOOGLE_PROJECT_ID = os.getenv('GOOGLE_PROJECT_ID')
+    GOOGLE_PRIVATE_KEY_ID = os.getenv('GOOGLE_PRIVATE_KEY_ID')
+    GOOGLE_PRIVATE_KEY = os.getenv('GOOGLE_PRIVATE_KEY').replace('\\n', '\n') 
+    GOOGLE_CLIENT_EMAIL = os.getenv('GOOGLE_CLIENT_EMAIL')
+    GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+    GOOGLE_AUTH_URI = os.getenv('GOOGLE_AUTH_URI')
+    GOOGLE_TOKEN_URI = os.getenv('GOOGLE_TOKEN_URI')
+    GOOGLE_AUTH_PROVIDER_X509_CERT_URL = os.getenv('GOOGLE_AUTH_PROVIDER_X509_CERT_URL')
+    GOOGLE_CLIENT_X509_CERT_URL = os.getenv('GOOGLE_CLIENT_X509_CERT_URL')
