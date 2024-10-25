5984e419b852186c7e81cb82e2aa41bdb7ee7574
Pascal Tuy <2022025tuy@aupp.edu.kh>
2024-10-25 11:55:48 +0700

    design update

--- frontend/src/components/Home/Banner.jsx
+++ frontend/src/components/Home/Banner.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
+import "../../assets/css/elements/banner.css";
 
 const Banner = () => {
     return (
-        <div className="banner bg-primary d-flex justify-content-center">
+        <div className="banner bg-primary d-flex justify-content-center align-items-center">
             <div>
                 <Link className="text-white" to="https://www.aupp.edu.kh/">AUPP
-                    <span>&nbsp;|&nbsp;</span>
-                    </Link>
-            </div>
-            <div>
-            <Link className="text-white" to="/">News & Events
                     <span>&nbsp;|&nbsp;</span>
                 </Link>
             </div>
             <div>
-            <Link className="text-white" to="https://www.aupp.edu.kh/student-life/">Student Life</Link>
+                <Link className="text-white" to="/">News & Events
+                    <span>&nbsp;|&nbsp;</span>
+                </Link>
+            </div>
+            <div>
+                <Link className="text-white" to="https://www.aupp.edu.kh/student-life/">Student Life</Link>
             </div>
         </div>
     );
