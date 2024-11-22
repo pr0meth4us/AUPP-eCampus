edf5e5aed19d564fa1aa8f4ea6432c0143b6cedc
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-11-22 12:36:30 +0700

    cofiguration bug

--- backend/services/mongo_service.py
+++ backend/services/mongo_service.py
@@ -1,13 +1,19 @@
 from pymongo import MongoClient
+import re
+from config import Config
+
 mongo_client = None
 db = None
 
 
-def init_mongo(app):
+def init_mongo():
     global mongo_client, db
-    mongo_uri = app.config['MONGO_URI']
+    mongo_uri = Config.MONGO_URI
+    print(f"MONGO_URI: {mongo_uri}")
+
     mongo_client = MongoClient(mongo_uri)
     db = mongo_client.get_database('auppEcampus')
+
     if db is not None:
         print('Mongo connected')
     else:
