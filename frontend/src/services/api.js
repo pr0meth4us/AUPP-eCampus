4b0fa572e57baacb89080ecb515203dde3fc348c
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-12-06 11:25:41 +0700

    present

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,34 +1,8 @@
 import axios from 'axios';
 import { API_BASE_URL } from "../config";
 
-const endpoint = (path = "") => {
-    const instance = axios.create({
-        baseURL: `${API_BASE_URL}${path ? `/${path}` : ''}`,
-        withCredentials: true,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    });
-
-    instance.interceptors.response.use(
-        response => response,
-        error => {
-            console.error('API Error:', error);
-
-            if (error.response) {
-
-            } else if (error.request) {
-                console.error('No response received');
-            } else {
-                console.error('Error', error.message);
-            }
-
-            return Promise.reject(error);
-        }
-    );
-
-    return instance;
-};
-
+const endpoint = (path = "") => axios.create({
+    baseURL: `${API_BASE_URL}/${path}`,
+    withCredentials: true
+});
 export default endpoint;
