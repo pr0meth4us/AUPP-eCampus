d0ac1cf4917d8e1589767bc7f9844c1ee892f5db
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-12-09 00:10:34 +0700

    fix cors

--- backend/services/cors_service.py
+++ backend/services/cors_service.py
@@ -7,7 +7,7 @@ def init_cors(app):
     cors.init_app(app, resources={r"/*": {
         "origins": [
             "http://localhost:3000",
-            "https://your-production-frontend.com",
+            "https://ecampusauppedu.vercel.app/",
             "http://127.0.0.1:3000",
             "http://192.168.100.58:3000",
         ],
