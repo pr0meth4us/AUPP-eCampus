9828dd5f8f3d2d398c44387294fbac52ab108708
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-12-09 00:30:29 +0700

    test cors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,9 @@ import { API_BASE_URL } from "../config";
 
 const endpoint = (path = "") => axios.create({
     baseURL: `${API_BASE_URL}/${path}`,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Access-Control-Allow-Origin': 'https://ecampusauppedu.vercel.app'
+    }
 });
 export default endpoint;
