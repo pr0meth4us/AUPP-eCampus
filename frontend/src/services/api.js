3e767a3478acb2d2f784a870a30077857dda26ac
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-12-09 00:50:28 +0700

    test cors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 import { API_BASE_URL } from "../config";
 
-const endpoint = (path = "") => axios.create({
-    baseURL: `${API_BASE_URL}/${path}`,
-    withCredentials: true,
-});
-endpoint.interceptors.request.use(config => {
-    config.url = config.url.replace('//', '/');
-    return config;
-});
+const endpoint = (path = "") => {
+    const instance = axios.create({
+        baseURL: `${API_BASE_URL}/${path}`,
+        withCredentials: true,
+    });
+
+    // Add a request interceptor
+    instance.interceptors.request.use(
+        config => {
+            if (config.url) {
+                config.url = config.url.replace(/([^:]\/)\/+/g, "$1"); // Ensure no double slashes
+            }
+            return config;
+        },
+        error => Promise.reject(error)
+    );
+
+    return instance;
+};
+
 export default endpoint;
